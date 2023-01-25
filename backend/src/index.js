e4751af58df13d4a1154b8b9d7b46fab14bf29f8
Yasser Angel <yasserangelgm@gmail.com>
2023-01-24 21:03:25 -0600

    Create database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const consola = require('./helpers/consola');
+const connectDB = require('./config/db');
+const cors = require('cors');
 const app = express();
 
+//Middlewares
 require('dotenv').config();
+app.use(cors());
 
+//Route imports
+
+//Routes
 app.get('/', (req, res) => res.send('Hello World!'));
+
 app.listen(process.env.PORT, () =>
   consola('FgCyan', `Servidor activo en el puerto: ${process.env.PORT}!`)
 );
+
+connectDB();

b163503ce4da935af14f8b56f4e9aadd0ed5a9de
Yasser Angel <yasserangelgm@gmail.com>
2023-01-26 23:41:32 -0600

    Add submit method in signup page with axios

--- frontend/src/pages/login/login.page.jsx
+++ frontend/src/pages/login/login.page.jsx
@@ -2,6 +2,7 @@ function LoginPage() {
   return (
     <div>
       <h1>Login Page</h1>
+      <p>{process.env.REACT_APP_DEV_BASE_URL}</p>
     </div>
   );
 }
--- frontend/src/pages/register/register.page.jsx
+++ frontend/src/pages/register/register.page.jsx
@@ -1,16 +1,70 @@
+import { useState } from 'react';
+import axios from '../../api/axios';
+
 function RegisterPage() {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        '/signup',
+        JSON.stringify(
+          {
+            name,
+            lastName,
+            email,
+            password,
+          },
+          {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true,
+          }
+        )
+      );
+      console.log(response.data);
+    } catch (err) {
+      if (!err?.response) {
+        console.log('No hay respuesta del servisor');
+      } else {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <div>
       <h1>Registro</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Nombre:</label>
-        <input type="text" id="name" autoComplete="off" required></input>
+        <input
+          type="text"
+          id="name"
+          autoComplete="off"
+          required
+          onChange={(e) => setName(e.target.value)}
+        ></input>
 
         <label htmlFor="lastname">Apellido:</label>
-        <input type="text" id="lastname" autoComplete="off" required></input>
+        <input
+          type="text"
+          id="lastname"
+          autoComplete="off"
+          required
+          onChange={(e) => setLastName(e.target.value)}
+        ></input>
 
         <label htmlFor="email">Correo electrónico:</label>
-        <input type="text" id="email" autoComplete="off" required></input>
+        <input
+          type="text"
+          id="email"
+          autoComplete="off"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
 
         <label htmlFor="password">Contraseña:</label>
         <input
@@ -18,7 +72,9 @@ function RegisterPage() {
           id="password"
           autoComplete="off"
           required
+          onChange={(e) => setPassword(e.target.value)}
         ></input>
+        <button>Registrar</button>
       </form>
     </div>
   );

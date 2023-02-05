aa9e3b5b671d105974c0376021f5d9e5025bc215
Yasser Angel <yasserangelgm@gmail.com>
2023-02-05 11:28:14 -0600

    Add origins urls

--- backend/src/config/allowed.js
+++ backend/src/config/allowed.js
@@ -1,3 +1,8 @@
-const allowed = ['http://localhost:3000', 'http://localhost:8000'];
+const allowed = [
+  'http://localhost:3000',
+  'http://localhost:8000',
+  'https://su-store-e26f6.web.app',
+  'https://yasserangelgm.github.io/u-camp-project-5',
+];
 
 module.exports = allowed;

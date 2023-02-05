813ecf6c3605283328ff569713eeeb5f8ad14b6f
Yasser Angel <yasserangelgm@gmail.com>
2023-02-05 17:30:16 -0600

    Change BASE_URL to the production url in axios context

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const DEV = true;
+const DEV = false;
 
 const BASE_URL = DEV
   ? process.env.REACT_APP_DEV_BASE_URL
-  : process.env.REACT_APP_PROD_BASE_URL;
+  : 'https://e-commerce-51af.onrender.com/api';
 
 export default axios.create({
   baseURL: BASE_URL,

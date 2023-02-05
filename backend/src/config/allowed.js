ae86055980028757c993afe8165f1db460bd5629
Yasser Angel <yasserangelgm@gmail.com>
2023-02-05 14:59:43 -0600

    Add Netifly origin

--- backend/src/config/allowed.js
+++ backend/src/config/allowed.js
@@ -1,8 +1,8 @@
 const allowed = [
   'http://localhost:3000',
   'http://localhost:8000',
-  'https://su-store-e26f6.web.app',
   'https://yasserangelgm.github.io/u-camp-project-5',
+  'https://su-store.netlify.app/',
 ];
 
 module.exports = allowed;

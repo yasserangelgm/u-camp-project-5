462d0c12e582b88afe6dad2f56a6aaa419748573
Yasser Angel <yasserangelgm@gmail.com>
2023-02-05 20:38:13 -0600

    Access-Control-Allow-Origin

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -8,10 +8,18 @@ const BASE_URL = DEV
 
 export default axios.create({
   baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
+  withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
   withCredentials: true,
 });

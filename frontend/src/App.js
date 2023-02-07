1e3f8b6651975b81c1010e9b4938fb0417ecd8d1
Yasser Angel <yasserangelgm@gmail.com>
2023-02-07 15:16:17 -0600

    Remove refresh-token process

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import LoginPage from './pages/login/login.page';
 import Root from './components/root/root.component';
 import RootAdmin from './components/root-admin/root-admin.component';
 import { HomePage } from './pages/home/home.page';
-import PersistLogin from './components/persist-login';
 import RequireAuth from './components/require-auth';
 import { Unauthorized } from './pages/unauthorized/unauthorized.page';
 import AdminHomePage from './pages/admin/home/admin-home.page';
@@ -20,43 +19,39 @@ function App() {
     /* Public routes */
     <BrowserRouter basename="/">
       <Routes>
-        <Route element={<PersistLogin />}>
-          <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
-            <Route index element={<HomePage />}></Route>
-            <Route path="/signup" element={<RegisterPage />}></Route>
-            <Route path="/login" element={<LoginPage />}></Route>
-            <Route path="/profile" element={<UserProfile />}></Route>
-            <Route path="/logout" element={<LogoOut />}></Route>
-            <Route path="/contact" element={<RegisterPage />}></Route>
-            <Route path="/products" element={<RegisterPage />}></Route>
+        <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+          <Route index element={<HomePage />}></Route>
+          <Route path="/signup" element={<RegisterPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/profile" element={<UserProfile />}></Route>
+          <Route path="/logout" element={<LogoOut />}></Route>
+          <Route path="/contact" element={<RegisterPage />}></Route>
+          <Route path="/products" element={<RegisterPage />}></Route>
+          <Route path="/products:productId" element={<RegisterPage />}></Route>
+          <Route path="unauthorized" element={<Unauthorized />}></Route>
+        </Route>
+
+        {/* Protected routes */}
+
+        <Route element={<RequireAuth />}>
+          <Route
+            path="/admin/dashboard"
+            element={<RootAdmin />}
+            errorElement={<ErrorPage />}
+          >
+            <Route index element={<AdminHomePage />}></Route>
             <Route
-              path="/products:productId"
-              element={<RegisterPage />}
+              path="/admin/dashboard/products"
+              element={<AdminProducts />}
             ></Route>
-            <Route path="unauthorized" element={<Unauthorized />}></Route>
-          </Route>
-
-          {/* Protected routes */}
-
-          <Route element={<RequireAuth />}>
             <Route
-              path="/admin/dashboard"
-              element={<RootAdmin />}
-              errorElement={<ErrorPage />}
-            >
-              <Route index element={<AdminHomePage />}></Route>
-              <Route
-                path="/admin/dashboard/products"
-                element={<AdminProducts />}
-              ></Route>
-              <Route
-                path="/admin/dashboard/categories"
-                element={<LoginPage />}
-              ></Route>
-              <Route path="/admin/dashboard/users" element={<Users />}></Route>
-            </Route>
+              path="/admin/dashboard/categories"
+              element={<LoginPage />}
+            ></Route>
+            <Route path="/admin/dashboard/users" element={<Users />}></Route>
           </Route>
         </Route>
+
         <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
     </BrowserRouter>
